495f126503565d91b13cb9fc36e4a5218f78c296
Starjed <biel123albuquerque@hotmail.com>
2022-03-25 21:39:07 -0400

    NomeAlunos

---
+++ Nome dos Aulnos.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+        <title>Nome dos Alunos</title>
+    </head>
+
+    <body>
+        $Nome1 = 'Jonathan soares'; 
+        $Nome2 = 'Alan';
+        $Nome3 = 'Kelly';
+        $Nome4 = 'Manoel';
+        $Nome5 = 'Eduador';
+        $Nome6 = 'Iago';
+        $Nome7 = 'Felipe';
+        $Nome8 = 'Mozart';
+        $Nome9 = 'Victor';
+        $Nome10 = 'Bruno';
+        $Nome11 = 'Walisson';
+        $Nome12 = 'Enio';
+        $Nome13 = 'Patricia';
+        $Nome14 = 'igor';
+
+        echo $Nome1"</b>";
+        echo $Nome2"</b>";
+        echo $Nome3"</b>";
+        echo $Nome4"</b>";
+        echo $Nome5"</b>";
+        echo $Nome6"</b>";
+        echo $Nome7"</b>";
+        echo $Nome8"</b>";
+        echo $Nome9"</b>";
+        echo $Nome10"</b>";
+        echo $Nome11"</b>";
+        echo $Nome12"</b>";
+        echo $Nome13"</b>";
+        echo $Nome14"</b>";
+        
+ 
+    </body>
+</html>
